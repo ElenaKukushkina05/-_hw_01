9d3beee6cca65a136ade5ec46f580440e194ef21
Elena <kukushkinaelena5@gmail.com>
2023-03-10 20:06:00 +0300

    Add tasks

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿// 1. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.WriteLine("Введите первое число: ");
+// int number1 = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine("Введите второое число: ");
+// int number2 = int.Parse(Console.ReadLine()!);
+
+// if(number1 > number2){
+//     Console.WriteLine($"{number1} Наибольшее число"); Console.WriteLine($"{number2} Наименьшее число");
+// }
+// if(number2 > number1){
+//     Console.WriteLine($"{number2} Наибольшее число"); Console.WriteLine($"{number1} Наименьшее число");
+// }
+// Console.ReadLine();
+
+
+
+// 2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.WriteLine("Введите первое число: ");
+// int number1 = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine("Введите второе число: ");
+// int number2 = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine("Введите третье число: ");
+// int number3 = int.Parse(Console.ReadLine()!);
+
+// int max = number1;
+
+// if(number1 > max) max = number1;
+// if(number2 > max) max = number2;
+// if(number3 > max) max = number3;
+
+// Console.Write("max = ");
+// Console.WriteLine(max);
+
+
+
+// 3. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка(number%2==0)).
+
+// Console.WriteLine("Введите число: ");
+// int number = int.Parse(Console.ReadLine()!);
+
+// if(number%2 == 0){
+//     Console.WriteLine($"{number} четное число");
+// }
+// if(number%2 ==1){
+//     Console.WriteLine($"{number} нечетное число");
+// }
+
+// Console.WriteLine();
+
+
+
+
+// 4. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// Console.Write("Введите число: ");
+// int number = int.Parse(Console.ReadLine()!);
+
+// int A = number;
+
+// for(A=1;A<=number;A++){
+//     if(A%2 == 0)
+//     Console.WriteLine($"{A}");
+// }
